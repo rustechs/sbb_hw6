5c0eab44f43d680b391b9932ba8568cf4cd05666
amcnicoll <anthony.mcnicoll@gmail.com>
2015-05-10 13:50:51 -0400

    Progress save on controller

--- scripts/controller.py
+++ scripts/controller.py
@@ -20,11 +20,14 @@
 
     Toss (State 4)
         If we have the ball, execute toss maneuvre and go back to Defend.
+
+    These are different from phases, which are retrieved from the game server.
 '''
 
-import sys, rospy, robot_interface, rospkg
+import sys, rospy, arm_interface, rospkg
 from math import pi
 from random import random
+from time import sleep
 from sbb_hw6.srv import *
 from game_server.srv import *
 from geometry_msgs.msg import Point, Quaternion, Pose
@@ -36,13 +39,43 @@ class Controller():
     # Initializes the controller as a ROS node which recieves commands.
     def __init__(self):
 
-        # Hard-coded settings for initial pose and table location
+        # This node must send initialization info to the game server. Do it now.
         rospy.loginfo('Initializing controller')
-        self.side = side
+        rospy.loginfo('Connecting to game server...')
+        impath = rospack.get_path('sbb_hw6') + '/img/logo.png'
+        img = cv2.imread(impath)
+        imMsg = cv_bridge.CvBridge().cv2_to_imgmsg(img, encoding="bgr8")
+        self.gs = rospy.ServiceProxy('/game_server/init', Init)
+        rospy.wait_for_service('/game_server/init')
+        resp = self.gs('Super Baxter Bros.', imMsg)
+        self.side = resp.arm
+
+        # This is a useful sign to have later
+        if self.side == 'left':
+            self.ys = -1
+        else:
+            self.ys = 1
+
+        # Get the number of blocks from a parameter
+        self.nBlocks = rospy.get_param('/num blocks')
+
+        # This node must also subscribe to the game server topic
+        rospy.Subscriber('/game_server/game_state', GameState, self.storeState)
+
+        # This node makes use of the find_stuff service
+        self.cv = rospy.ServiceProxy('find_stuff', FindStuffSrv)
+
+        # Initialize our arm
+        rospy.loginfo('Connecting to Baxter...')
+        self.arm = arm_interface.Arm(self.side)
+        self.arm.enable()
+
+        # Hard-coded settings for initial pose and table location
+        self.blockSpacing = .1
         self.searchD = .05
         self.tableZ = -.07
         self.scanZ = .3
-        self.safeZ = .08
+        self.safeZ = self.tableZ + self.nBlocks*.044
         self.xyTol = .01
         self.thetaTol = .3
         self.controlGain = .5
@@ -52,33 +85,17 @@ class Controller():
         self.camOffset = .035
         self.xmin = .3
         self.ymin = {right: -.6, left: .1}
+        self.ymin = self.ymin[self.side]
         self.xmax = .8
         self.ymax = {right: -.1, left: .6}
+        self.ymax = self.ymax[self.side]
+
+        self.phase = 0
+        self.state = 0
 
         # Define the useful "downwards" quaternion
         self.downwards = self.e2q(0, pi, 0)
 
-        # initialize the Baxter object
-        # The bowl should be within the right arm's reachable workspace on table
-        rospy.loginfo('Connecting to Baxter...')
-        self.baxter = robot_interface.Baxter()
-        self.baxter.enable()
-        rospy.loginfo('Connected to Baxter successfully.')
-
-        # This node must send initialization info to the game server. Do it now.
-        impath = rospack.get_path('sbb_hw6') + '/img/logo.png'
-        img = cv2.imread(impath)
-        imMsg = cv_bridge.CvBridge().cv2_to_imgmsg(img, encoding="bgr8")
-        self.gs = rospy.ServiceProxy('/game_server/init', Init)
-        rospy.wait_for_service('/game_server/init')
-        resp = self.gs('Super Baxter Bros.', imMsg)
-        self.side = resp.arm
-
-        # This node must also subscribe to the game server topic
-        rospy.Subscriber('/game_server/game_state', GameState, self.storeState)
-
-        # This node makes use of the find_stuff service
-        self.cv = rospy.ServiceProxy('find_stuff', FindStuffSrv)
         rospy.loginfo('Initialization successful.')
 
     # Convenience function for euler_to_quaternion that returns as Quaternion.
@@ -92,10 +109,10 @@ class Controller():
         return self.rerange(eu[2] + pi)
 
     # Convenience function for changing the angular alignment in YZ only
-    def setW(self, side, w):
-        p = self.baxter.getEndPose(side)
+    def setW(self, w):
+        p = self.arm.getEndPose()
         p.orientation = self.e2q(0, pi, w)
-        self.baxter.setEndPose(side,p)
+        self.arm.setEndPose(p)
 
     # Puts a value in the -pi to pi range
     def rerange(self, val):
@@ -110,19 +127,41 @@ class Controller():
     # It assumes/requires a downward orientation.
     # It takes care of checking the height off the table itself.
     def pix2m(self, pix):
-        ps = self.baxter.getEndPose('right')
+        ps = self.arm.getEndPose()
         height = ps.position.z - self.tableZ
         return (pix * (height/self.pixCalZ) * self.pixCalD) / self.pixCalN
 
     # This allows the user to teach positions using the grip buttons.
-    def calibPose(self, side):
-        # Define the "goalpost" positions TODO
-        self.goal1 = 0
-        self.goal2 = 0
+    def calibPose(self):
+
+        # Get some positions from the user
+        goal = [None, None]
+        rospy.loginfo('Place arm over left goalpost and press cuff...')
+        self.arm.waitForCuff()
+        self.goal[0] = self.arm.getEndPose()
+
+        rospy.loginfo('Place arm over right goalpost and press cuff...')\
+        self.arm.waitForCuff()
+        self.goal[1] = self.arm.getEndPose()
+
+        rospy.loginfo('Place arm over top of stack and press cuff...')\
+        self.arm.waitForCuff()
+        self.stackTop = self.arm.getEndPose()
+
+        rospy.loginfo('Place arm over field center and press cuff...')\
+        self.arm.waitForCuff()
+        self.center = self.arm.getEndPose()
+
+        # Pre-calculate the x/y spacing of blocks
+        self.blockY = self.center.position.y + self.ys*.2
+        width = (self.nBlocks-1)*self.blockSpacing
+        self.blockX = range(self.bBlocks)
+        for x in self.blockX:
+            x = x*self.blockSpacing + self.center.position.x - width/2
 
     # This listens to the state publishing topic and remembers the state info internally
     def storeState(self, data):
-        self.state = data.current_phase
+        self.phase = data.current_phase
 
     # Get a response from CV service: found, x, y, theta (theta doesn't matter)
     # x, y are converted to meters before returning
@@ -147,48 +186,48 @@ class Controller():
     # Coordinates pick-place action done by Baxter
     # "from" and "to" need to be poses of the end link
     # Since we are AGAIN doing upright orientation only, neglect end effector
-    def pickPlace(self, side, romP, toP):
+    def pickPlace(self, fromP, toP):
 
         rospy.loginfo('Performing pick/place.')
 
         # Make safe pre-pick pose and go
         ps = Pose(Point(fromP.position.x, fromP.position.y, self.safeZ), fromP.orientation)
-        self.baxter.openGrip(side)
-        self.baxter.setEndPose(side, ps)
+        self.arm.openGrip()
+        self.arm.setEndPose(ps)
 
         # Go to pick and pick
-        self.baxter.setEndPose(side, fromP)
-        self.baxter.closeGrip(side)
+        self.arm.setEndPose(fromP)
+        self.arm.closeGrip()
 
         # Path to place pose through safe points
-        self.baxter.setEndPose(side, ps)
+        self.arm.setEndPose(ps)
         ps = Pose(Point(toP.position.x, toP.position.y, self.safeZ), toP.orientation)
-        self.baxter.setEndPose(side, ps)
-        self.baxter.setEndPose(side, toP)
+        self.arm.setEndPose(ps)
+        self.arm.setEndPose(toP)
 
         # Finish place and back off to safe height
-        self.baxter.openGrip(side)
-        self.baxter.setEndPose(side, ps)
+        self.arm.openGrip()
+        self.arm.setEndPose(ps)
         rospy.loginfo('Pick/place complete.')
 
     # This function attempts to put the end effector within sight of the ball centroid.
     # If the current end pose does not accomplish this, it moves to a random pose in the 
     # search plane, and checks again. Call repeatedly until succesful.
-    def scanForBall(self, side):
+    def scanForBall(self):
         ball = self.getBall()
         if ball.found:
             rospy.loginfo('Ball found.')
             return ball
         else:
             xpos = self.xmin + (self.xmax - self.xmin)*random()
-            ypos = self.ymin[side] + (self.ymax[side] - self.ymin[side])*random()
+            ypos = self.ymin + (self.ymax - self.ymin)*random()
             ps = Pose(Point(xpos, ypos, self.scanZ), self.downwards)
-            self.baxter.setEndPose(side, ps)
+            self.arm.setEndPose(ps)
 
     # Same as scanForBall, except that the range of motion is restricted to the bowl's
     # diameter, about the initial point. Checks for exceeding xmin/xmax aren't implemented.
-    def scanForBlock(self, side):
-        start = self.baxter.getEndPose('right')
+    def scanForBlock(self):
+        start = self.arm.getEndPose()
         while True:
             block = self.getBlock()
             if block.found:
@@ -198,12 +237,12 @@ class Controller():
                 xpos = start.position.x + self.searchD*(random() - .5)
                 ypos = start.position.y + self.searchD*(random() - .5)
                 ps = Pose(Point(xpos, ypos, self.scanZ), self.downwards)
-                self.baxter.setEndPose(side, ps)
+                self.arm.setEndPose(ps)
 
     # This function must be run only when we are reliably in range of an object
     # It performs choppy closed-loop semi-proportional control of the end effector
     # until it is aligned and directly over the object.
-    def controlTo(self, side, objective):
+    def controlTo(self, objective):
 
         rospy.loginfo('Controlling to %s center...' % objective)
 
@@ -221,7 +260,7 @@ class Controller():
 
         # Control loop. Never. Give. Up.
         while True:
-            ps = self.baxter.getEndPose(side)
+            ps = self.arm.getEndPose()
             obj = objfun()
             if not obj.found:
                 rospy.loginfo('Lost %s! Aborting control.' % objective)
@@ -232,12 +271,12 @@ class Controller():
                 rospy.loginfo('Center control complete.')                
                 return ps
             newx = clamp(ps.position.x + (obj.x * self.controlGain), self.xmin, self.xmax)
-            newy = clamp(ps.position.y + (obj.y * self.controlGain), self.ymin[side], self.ymax[side])
+            newy = clamp(ps.position.y + (obj.y * self.controlGain), self.ymin, self.ymax)
             ps = Pose(Point(newx, newy, ps.position.z), self.downwards)
-            self.baxter.setEndPose(side, ps)
+            self.arm.setEndPose(ps)
 
     # This does the rotational alignment control.
-    def alignWith(self, side, objective):
+    def alignWith(self, objective):
 
         rospy.loginfo('Aligning with %s ...' % objective)
         delTheta = 1000
@@ -250,7 +289,7 @@ class Controller():
 
         # Control loop. Never. Give. Up.
         while True:
-            ps = self.baxter.getEndPose('right')
+            ps = self.arm.getEndPose()
             obj = objfun()
             if not obj.found:
                 rospy.loginfo('Lost %s! Aborting control.' % objective)
@@ -263,16 +302,50 @@ class Controller():
                 rospy.loginfo('Alignment complete.')                
                 return ps
             newt = self.rerange(self.getW(ps)- obj.t)
-            self.setW(side, newt)
+            self.setW(newt)
 
-    # Performs the sequence of actions required to accomplish HW5 CV objectives.
-    def execute(self):
+    # Performs the sequence of actions required to accomplish HW6 objectives.
+    def playGame(self):
+
+        # Wait until the game starts
+        rospy.loginfo('Waiting for game to start...')
+        while 1
+            if self.phase > 0
+                break
+            sleep(.005)
+
+        # First, calibrate some locations on the field.
+        self.calibPose()
+
+        # Wait until the game phase is 2 (block stacking)
+        rospy.loginfo('Calibration done. Waiting for next game phase...')
+        while 1
+            if self.phase > 1
+                break
+            sleep(.005)
+
+        # Unstack the blocks onto the table
+        for bl in range(self.nBlocks):
+            fromP = self.stackTop
+            fromP.position.z = fromP.position.z - bl*.044
+            toP = Pose(Point(self.blockX[bl], self.blockY, self.tableZ), self.downwards)
+            self.pickPlace(fromP, toP)
+
+        # Wait until the game phase is 3 (main game)
+        rospy.loginfo('Stacking done. Waiting for next game phase...')
+        while 1
+            if self.phase > 2
+                break
+            sleep(.005)
+
+        # Enter the main state loop
+
         self.scanForBowl()
         self.controlTo('bowl')
         self.scanForBlock()
-        ps = self.baxter.getEndPose('right')
+        ps = self.arm.getEndPose()
         ps.position.z = self.safeZ
-        self.baxter.setEndPose('right', ps)
+        self.arm.setEndPose(ps)
         bPose = self.controlTo('block')
         bPose = self.alignWith('block')
         bPose.position.z = self.tableZ
@@ -282,11 +355,9 @@ class Controller():
 # This is what runs when the script is executed externally.
 # It runs the main node function, and catches exceptions.
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print "You need to specify a side!"
     try:
-        controller = Controller(sys.argv[1])
-        controller.execute()
+        controller = Controller()
+        controller.playGame()
     except:
         import pdb, traceback, sys
         type, value, tb = sys.exc_info()
